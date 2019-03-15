b3d71d96f652a22fdb781b0514cfd3a13411571b
rakesh <adepu.rakesh@gmail.com>
2019-03-14 21:16:41 -0400

    modified test

--- src/test/java/com/smarts/qa/testcases/LoginTest1.java
+++ src/test/java/com/smarts/qa/testcases/LoginTest1.java
@@ -6,11 +6,23 @@ import com.smarts.qa.page.LoginPage;
 import com.smarts.qa.util.TestNgAnnotations;
 
 public class LoginTest1 extends TestNgAnnotations {
-	
+	String d="jahjkfhkahf";
+			
 
 	@Test
 	public void loginTesting() {
+		
 		LoginPage login = new LoginPage(driver);
+		
+
+		login.LogintoOpenCart();
+
+	}
+	@Test
+	public void loginTesting3() {
+		
+		LoginPage login = new LoginPage(driver);
+		
 
 		login.LogintoOpenCart();
 
